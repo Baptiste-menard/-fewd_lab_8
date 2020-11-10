627b131e217f73d79dc5e13a8f1ad29899dac012
Baptiste-menard <baptistemn@gmail.com>
2020-11-10 16:43:07 +0000

    ‘commit

---
+++ baptiste.js
@@ -0,0 +1,59 @@
+// Question 3 & 4
+
+
+var planets = [ 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus'];
+console.log(planets.indexOf('Earth'));
+
+// Question 5
+var a = 5, b = 'Hello', c = 'World!', d = 10;
+a + b;
+console.log(a + ' ' + b);
+b + c;
+console.log(b + ' ' + c);
+a + d;
+console.log(a + d);
+
+// Question 6
+
+var a = 5, b = 10, c = "5";
+var x = a;
+a == c;
+console.log(a == c);
+a === c;
+console.log(a === c);
+a == x;
+console.log(a == x);
+a != b;
+console.log(a != b);
+a > b;
+console.log(a > b);
+a < b;
+console.log(a < b);
+a >= b;
+console.log(a >= b);
+a <= b;
+console.log(a <= b);
+a >= c;
+console.log(a >= c);
+a <= c;
+console.log(a <= c);
+
+
+// Question 7 & 8
+
+var Hello = function() {
+    return 'Hello World';
+};
+var result = Hello();
+console.log(result);
+
+
+var hello = function() {
+    return 'Hi!';
+};
+
+var x = hello ();
+console.log(x);
+
+
+
